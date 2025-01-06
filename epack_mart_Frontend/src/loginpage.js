c6c17be6052fb5c1b4460cf2e605687d9e10c814
Vrushabh Ugale <vrushabhugale21@gmail.com>
2025-01-06 19:09:18 +0530

    djdk file created by vrushabh

---
+++ epack_mart_Frontend/src/loginpage.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+import { useReducer } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from './slice';
+//import { json } from 'body-parser';
+
+const LoginPage = () => {
+  const init = {
+    email: '',
+    password: ''
+  }
+
+  const reducer=(state, action)=> {
+    switch (action.type) {
+      case 'UPDATE':
+        return { ...state, [action.name]: action.value }
+      case 'RESET':
+        return init;
+    }
+  }
+
+  const [data, dispatch] = useReducer(reducer, init);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+    const dispatch1= useDispatch();
+  
+  //const reduxAction = useDispatch();
+
+  const handelSubmit = (e) =>{
+    e.preventDefault();
+    const request ={
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        { email_id: data.email, 
+          password: data.password 
+        })
+    }
+
+    fetch("http://localhost:8080/login", request)
+    .then (response => {
+            if (response.ok)
+              return response.json();
+            else
+              throw new Error(response.statusText);
+            })
+    .then(resp=>{ 
+      console.log(resp);
+      return resp.length>0 ? JSON.parse(resp) : {}})
+    .then (obj => {
+      console.log(obj);
+                 if(Object.keys(obj).length === 0){
+                  setError("Invalid Inputs... Try again")
+                 }
+                 else{
+                  //reduxAction(login())
+                  if(obj.role_id.role_id === 3){
+                    //dispatch(login());
+                    navigate("/");
+                  }
+                  else if(obj.role_id.role_id === 2){
+                    navigate("/");
+                  }
+                 }
+                }
+              )
+              .catch((error)=>alert("Server Error..."))
+  }
+
+  
+
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <form onSubmit={handelSubmit}>
+        {/* Email Field */}
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={(e)=> dispatch({type:"UPDATE", name:"email", value:e.target.value})}
+            required
+          />
+        </div>
+
+        {/* Password Field */}
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={(e)=> dispatch({type:"UPDATE", name:"password", value:e.target.value})}
+            required
+          />
+        </div>
+        {JSON.stringify(data)}
+
+        {/* Error Message */}
+        {error && <p className="error-message">{error}</p>}
+
+        {/* Submit Button */}
+        <button type="submit">Login</button>
+      </form>
+
+      {/* Forgot Password Link */}
+      <p className="forgot-password">
+        <a href="/forgot-password">Forgot Password?</a>
+      </p>
+    </div>
+  );
+};
+
+export default LoginPage;
