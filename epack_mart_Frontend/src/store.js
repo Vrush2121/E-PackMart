ffd5b96889ae9447fb9d4d7941d52a72aa2e523c
Vrushabh Ugale <vrushabhugale21@gmail.com>
2025-01-07 18:39:45 +0530

    db added

--- epack_mart_Frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { loggedReducer, loggedSlice } from "./slice";
-
-export default configureStore({
-    reducer : {
-        // logged : loggedSlice.reducer
-        loggedin:false
-    }
-})
